362d19cf64f4e5cbe3801e848689fa267fb6d797
Jeffrey Sanford <jeffreysanford@gmail.com>
2015-05-21 17:51:22 -0400

    wrapping javascript

--- javascript/show-hide-content/javascript.js
+++ javascript/show-hide-content/javascript.js
@@ -1,7 +1,7 @@
 (function () {
-   /* 
-   This function will show information on the page if a 
-   checkbox is clicked.  If it is not clicked th, it will 
+   /*
+   This function will show information on the page if a
+   checkbox is clicked.  If it is not clicked th, it will
    hide the informaion.
    */
    // listen for this event
@@ -16,23 +16,23 @@
       content.style.display = 'block';
     }
   });
+  //  Example of a closure
+  //  http://javascriptissexy.com/understand-javascript-closures-with-ease/
+
+  function showName (firstName, lastName) {
+    // this inner function has access to the outer
+    // function's variables, including the parameter
+    function makeFullName () {
+      var wholeName = firstName + ' ' + lastName;
+      console.log("Changing Full Name (#userProfileName) to: " + wholeName );
+      return wholeName;
+    }
+  return makeFullName ();
+  }
+
+  var userProfileName = document.getElementById('userProfileName');
+
+  // Insert Jim Thompson into the DOM user profile name
+  userProfileName.innerHTML = showName ('Bob', 'Zindel');
 }());
 
-//  Example of a closure
-//  http://javascriptissexy.com/understand-javascript-closures-with-ease/
-
-function showName (firstName, lastName) {
-  // this inner function has access to the outer 
-  // function's variables, including the parameter​
-  function makeFullName () {
-    var wholeName = firstName + ' ' + lastName;
-    console.log("Changing Full Name (#userProfileName) to: " + wholeName );
-    return wholeName;
-  }
-return makeFullName ();  
-}
-
-var userProfileName = document.getElementById('userProfileName');
-
-// Insert Jim Thompson into the DOM user profile name
-userProfileName.innerHTML = showName ('Bob', 'Zindel'); 
